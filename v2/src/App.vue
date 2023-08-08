8ec719a709e1b4b7c80aafa394954d4b22d3cf16
zoselaslwl <smj980902@gmail.com>
2023-08-08 15:20:02 +0900

    smj

---
+++ v2/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <img id="logo" src="./assets/북로고.png" alt="Logo" />
+    <nav>
+      <router-link to="/Home">Home</router-link>
+      <router-link to="/BestSeller">Best 도서</router-link>
+      <router-link to="/recommendedReading">추천 도서</router-link>
+      <router-link to="/NewBooks">신간</router-link>
+      <router-link to="/bookCategory">카테고리</router-link>
+      <router-link to="/bookInformation">책정보</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  background-color: #ffffff;
+  padding: 20px;
+}
+
+nav {
+  padding: 20px;
+  font-size: 18px;
+  background-color: #fff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  margin-bottom: 30px;
+}
+
+nav a {
+  font-weight: 600;
+  color: #2c3e50;
+  text-decoration: none;
+  margin: 0 15px;
+  padding: 8px 12px;
+  border-radius: 4px;
+  transition: background-color 0.3s ease-in-out, color 0.3s ease-in-out;
+}
+
+nav a:hover {
+  background-color: #dfdfdf;
+  color: #fff;
+}
+
+#logo {
+  width: 250px;
+  height: 100px;
+  margin-bottom: 30px;
+  border-radius: 8px;
+}
+
+.router-link-exact-active {
+  background-color: #acacac;
+  color: #ffffff;
+}
+</style>
---
+++ v2/src/router/index.js
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/BestSeller',
+    name: 'BestSeller',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/BestSeller.vue')
+  },
+  {
+    path: '/recommendedReading',
+    name: 'recommended reading',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/recommendedReading.vue')
+  },
+  {
+    path: '/NewBooks',
+    name: 'NewBooks',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/NewBooks.vue')
+  },
+  {
+    path: '/category',
+    name: 'category',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/bookCategory.vue')
+  },
+  {
+    path: '/bookInformation',
+    name: 'bookInformation',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/bookInformation.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
