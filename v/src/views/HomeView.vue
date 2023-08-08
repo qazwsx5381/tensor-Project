9bf24025fa8e3297b8f74f7a3057a01e50bc9eb7
SCJ1231 <zkemdbgml@gmail.com>
2023-08-08 11:36:45 +0900

    수정

--- v/src/views/HomeView.vue
+++ v/src/views/HomeView.vue
@@ -1,5 +1,36 @@
 <template>
-  <div></div>
+  <div id="container">
+    <div id="nav"></div>
+    <div id="best"></div>
+    <div id="age"></div>
+    <div id="hot"></div>
+  </div>
+  <div id="foot"></div>
 </template>
 <script></script>
-<style></style>
+<style>
+#container {
+  display: flex;
+  background-color: black;
+}
+
+#nav {
+  background-color: aqua;
+}
+
+#best {
+  background-color: red;
+}
+
+#age {
+  background-color: blue;
+}
+
+#hot {
+  background-color: green;
+}
+
+#foot {
+  background-color: gold;
+}
+</style>
