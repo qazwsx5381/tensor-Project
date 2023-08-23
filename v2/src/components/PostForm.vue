74adb01ab6c36a027180fc2eed38c0182c097a04
zoselaslwl <smj980902@gmail.com>
2023-08-23 16:36:01 +0900

    20230823

---
+++ v2/src/components/PostForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>게시판</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  }
+}
+</script>
---
+++ v2/src/proxy-server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const request = require('request')
+const app = express()
+
+app.get('/NewBooks', (req, res) => {
+  const url =
+    'http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbzoselalswl981112001&QueryType=ItemNewAll&Cover=Big&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101'
+
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error)
+      res.status(500).json({ error: 'Internal Server Error' })
+      return
+    }
+
+    res.header('Access-Control-Allow-Origin', '*') // CORS 설정
+    res.send(body)
+  })
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+})
