a9e55cbb1d5f8c99ac5bad079b45f2c7510bd8ba
SCJ1231 <zkemdbgml@gmail.com>
2023-08-23 15:45:10 +0900

    230823

--- v/src/components/Footer.vue
+++ v/src/components/Footer.vue
@@ -1,12 +1,38 @@
 <template>
-  <footer>명언</footer>
+  <div>
+    <h1>Random Quote</h1>
+    <div id="quote">
+      <p>{{ currentQuote.name }}</p>
+      <p>- {{ currentQuote.text }}</p>
+    </div>
+    <button @click="randomQuote">다른 명언 보기</button>
+  </div>
 </template>
-<script></script>
-<style scoped>
-  footer {
-        text-align: center;
-        padding: 25px;
-        border: 1px solid gray;
-        background-color: cornflowerblue;
+
+<script>
+import quotes from '@/Quotes.js'
+
+export default {
+  name: 'RandomQuoteComponent',
+  data() {
+    return {
+      quotes: quotes,
+      currentQuote: {}
     }
-</style>
+  },
+  methods: {
+    getRandomQuote() {
+      const randomIndex = Math.floor(Math.random() * this.quotes.length)
+      return this.quotes[randomIndex]
+    },
+    randomQuote() {
+      // 새로운 랜덤 명언 표시
+      this.currentQuote = this.getRandomQuote()
+    }
+  },
+  mounted() {
+    // 초기에 랜덤 명언을 표시
+    this.randomQuote()
+  }
+}
+</script>
